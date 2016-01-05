8baeb3362a14aaf971a62727be8898163c9fda31
jbsplunk <jboucher@splunk.com>
2016-01-05 11:08:53 -0600

    Add access generator instruction images

--- bin/access_generator.py
+++ bin/access_generator.py
@@ -1,15 +1,16 @@
-"""
+
+'''
 access_generator.py
 Needed to develop initial access tokens for script use.
 Currently in Beta version. Email me to get instructions on
 usage. I need to automate process still. - JB
-"""
+'''
 
 import os, fitbit, json
 import cherrypy
 import webbrowser
-# import requests.packages.urllib3
-# requests.packages.urllib3.disable_warnings()
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 # Setup Splunk Environment
 APPNAME = 'Splunk_TA_fit'
